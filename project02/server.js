ab717f77f270d5358b1465848b83f87df056254a
rostislav-nikitin90 <rosnikitin90@gmail.com>
2023-02-25 21:57:09 +0100

    Completed 08 Personal Assignment

--- project02/server.js
+++ project02/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080;
 const app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
 
 app
   .use(bodyParser.json())
@@ -10,7 +11,26 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', require('./routes'));
+  .use('/', require('./routes'))
+  .use(
+    auth({
+      authRequired: false,
+      auth0Logout: true,
+      issuerBaseURL: process.env.ISSUER_BASE_URL,
+      baseURL: process.env.BASE_URL,
+      clientID: process.env.CLIENT_ID,
+      secret: process.env.SECRET,
+      idpLogout: true,
+    })
+  );
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 const db = require('./models');
 db.mongoose
